c23cd2e0eb7481ead6f1b1c5d4d2c46954a787d0
Willy Devin Aufa Amanulloh <Willy.devin.stu@pnc.ac.id>
2024-06-23 23:31:26 +0700

    cek

---
+++ app/Http/Controllers/ImportUserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Imports\UsersImport;
+use Maatwebsite\Excel\Facades\Excel;
+
+class ImportUserController extends Controller
+{
+    public function showForm()
+    {
+        return view('import');
+    }
+
+    public function import(Request $request)
+    {
+        $request->validate([
+            'file' => 'required|mimes:xlsx,xls,csv'
+        ]);
+
+        Excel::import(new UsersImport, $request->file('file'));
+
+        return redirect()->back()->with('success', 'Users imported successfully!');
+    }
+}
---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\User;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Illuminate\Support\Facades\Hash;
+
+class UsersImport implements ToModel, WithHeadingRow
+{
+    public function model(array $row)
+    {
+        return new User([
+            'nama' => $row['nama'],
+            'npm_nidn_npak' => $row['npm_nidn_npak'],
+            'email' => $row['email'],
+            'password' => Hash::make($row['password']),
+        ]);
+    }
+}
